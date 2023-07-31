04b5b2ed4ad08a9953f8da8c71bcbd6c828bd8d7
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-07-31 17:31:59 +0900

    +

--- client04/src/App.js
+++ client04/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Test from './Test';
+import Join from './Join';
+
 
 
 function App() {
   return (
     <div className="App">
-    <Test></Test>
-
+    <h2>Hello, React</h2>
+    <Test></Test><br/>
+    <Join></Join>
     </div>
   );
 }
---
+++ client05/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import A from "./Component/A";
+import B from "./Component/B";
+import C from "./Component/C";
+
+import {Routes,Route} from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/A" element={<A />}/>
+      <Route path="/B" element={<B />}/>
+      <Route path="/C" element={<C />}/>
+    </Routes>
+  );
+}
+export default App;
