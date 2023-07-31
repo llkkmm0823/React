7a777fe5f789f2028d2694c2e818b5eb52e876c0
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-07-31 16:12:21 +0900

    +

---
+++ client03/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Test';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello, React</h2>
+      <Test></Test>
+    </div>
+  );
+}
+
+export default App;
