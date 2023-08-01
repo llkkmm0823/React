0efd85cb41ceedffa4752596a5600f6023b3dd18
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-08-01 09:43:09 +0900

    ++

--- client05/src/App.js
+++ client05/src/App.js
@@ -1,20 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import A from "./Component/A";
 import B from "./Component/B";
 import C from "./Component/C";
-
-import {Routes,Route} from "react-router-dom";
-
-
+import {  Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/A" element={<A />}/>
-      <Route path="/B" element={<B />}/>
-      <Route path="/C" element={<C />}/>
+        <Route path="/" element={<A />} />
+        <Route path="/B" element={<B />} />
+        <Route path="/C" element={<C />} />
     </Routes>
   );
 }
+
 export default App;
